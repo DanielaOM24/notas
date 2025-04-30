0980fe3aea7a7668585b23200e2f4359008726ec
DanielaOM24 <danielaorrego4@outlook.com>
2025-04-30 12:33:52 -0500

    Update taller2.py

--- taller2.py
+++ taller2.py
@@ -3,12 +3,13 @@ print("."*30)
 print("Bienvenido al sistema de calificaciones")
 print("."*30)
 
-def determinar_estado():
 
    #Usamos este bloque para manejar errores.
-    try:
+while True:
+ try:
             #SE SOLICITA AL USUARIO INGRESAR DATO   
             nota=float(input("ingrese una calificación entre 0 y 100:"))
+            print("."*30)
             #verificar rango correcto
             if 0 <= nota <=100:
                 if nota >=60:
@@ -17,10 +18,18 @@ def determinar_estado():
                     print("El estudiante reprobó el curso")    
             else:
                 print("ERROR:ingrese un número permitido en el rango de 0 a 100")        
-    except ValueError:
+                break
+ except ValueError:
         print ("Error:ingresa un numero valido")
+        print("."*30)
  
     # se pide lista de notas al usuario separada por comas
-    lista= input("Ingresa una lista de calificaciones separadas por comas:")
+ lista= input("Ingresa una lista de calificaciones separadas por comas:")
     #se convierte esa cadena  en una lista de numeros 
-    notas=
+ notas= [float(x.strip()) for x in lista.split(",")]
+ #se hace ṕara el promedio 
+ promedio= sum(notas)/ len(notas)
+ print(f"El promedio de las calificaciones es:{promedio}")
+
+
+______________________________
